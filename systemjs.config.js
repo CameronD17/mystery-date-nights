5ea70a0e7dd393f9b1a533eb147feebb955becb8
CameronD17 <camerondoyle@live.co.uk>
2016-10-15 22:33:15 +0100

    updated package.json

--- systemjs.config.js
+++ systemjs.config.js
@@ -21,10 +21,8 @@
       '@angular/http': 'npm:@angular/http/bundles/http.umd.js',
       '@angular/router': 'npm:@angular/router/bundles/router.umd.js',
       '@angular/forms': 'npm:@angular/forms/bundles/forms.umd.js',
-      // other libraries
       'rxjs':                      'npm:rxjs',
       'angular-in-memory-web-api': 'npm:angular-in-memory-web-api',
-      leaflet: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/leaflet.js'
     },
     // packages tells the System loader how to load when no filename and/or no extension
     packages: {

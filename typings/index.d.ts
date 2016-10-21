153f0ee365fd0b18bb7b8427a5f394e70b8dbd36
CameronD17 <camerondoyle@live.co.uk>
2016-10-21 10:13:01 +0100

    Refreshed gitignore

--- typings/index.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference path="globals/core-js/index.d.ts" />
-/// <reference path="globals/jasmine/index.d.ts" />
-/// <reference path="globals/node/index.d.ts" />
-/// <reference path="globals/geojson/geojson.d.ts" />
-/// <reference path="globals/leaflet/leaflet.d.ts" />

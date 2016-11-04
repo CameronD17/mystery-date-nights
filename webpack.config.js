43dcd13628bb71155c32a03823447595de368937
CameronD17 <camerondoyle@live.co.uk>
2016-11-04 17:36:16 +0000

    Added dog-friendly icons

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,9 @@ module.exports = {
     },
     module: {
         loaders: [
-            { test: /\.tsx?$/, loader: 'ts-loader' }
+            { test: /\.tsx?$/, loader: 'ts-loader' },
+            { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=10000&minetype=application/font-woff" },
+            { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader" }    
         ]
     }
 }
